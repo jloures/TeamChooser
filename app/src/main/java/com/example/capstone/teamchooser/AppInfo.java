9d1612d9a1a6a1cc74e8ec4eb2d0bed23713e10e
jloures <john_loures@hotmail.com>
2016-06-19 01:37:54 -0400

    Added info page to the app

---
+++ app/src/main/java/com/example/capstone/teamchooser/AppInfo.java
@@ -0,0 +1,23 @@
+package com.example.capstone.teamchooser;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class AppInfo extends AppCompatActivity {
+
+    private Toolbar m_toolbar = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_info);
+        m_toolbar = (Toolbar) findViewById(R.id.app_info_toolbar);
+        setSupportActionBar(m_toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
+    }
+}
--- app/src/main/java/com/example/capstone/teamchooser/MainActivity.java
+++ app/src/main/java/com/example/capstone/teamchooser/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.capstone.teamchooser;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar m_toolbar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        m_toolbar = (Toolbar) findViewById(R.id.create_game_toolbar);
+        setSupportActionBar(m_toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.add_game:
+                //We should go to another activity to add the game
+                break;
+
+            case R.id.get_info:
+                this.callAppInfoActivity();
+                break;
+
+            default:
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    private void callAppInfoActivity() {
+        Intent intent = new Intent(this, AppInfo.class);
+        startActivity(intent);
+
     }
 }
